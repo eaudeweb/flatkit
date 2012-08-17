4dbd7c4dec5d7293156f70c5fa90edc85a35d738
Alex Morega <alex@grep.ro>
2012-08-17 15:45:00 +0300

    extract common setUp

--- tests/table_enum_test.py
+++ tests/table_enum_test.py
@@ -5,19 +5,20 @@ import flask_htables
 
 class TableEnumTest(unittest.TestCase):
 
+    def setUp(self):
+        self.app = flask.Flask(__name__)
+        self.app.config.update(HTABLES_ENGINE='sqlite',
+                               HTABLES_SQLITE_PATH=':memory:')
+        self.ht = flask_htables.HTables(self.app)
+
     def test_two_values_fetched_from_database(self):
         from flatkit import ValuesFromTable
 
         class Thing(object):
             valid_values = ValuesFromTable('person', field='slug')
 
-        app = flask.Flask(__name__)
-        app.config.update(HTABLES_ENGINE='sqlite',
-                          HTABLES_SQLITE_PATH=':memory:')
-        ht = flask_htables.HTables(app)
-
-        with app.test_request_context():
-            person_table = ht.session['person']
+        with self.app.test_request_context():
+            person_table = self.ht.session['person']
             person_table.create_table()
             person_table.new(slug='red')
             person_table.new(slug='blue')
